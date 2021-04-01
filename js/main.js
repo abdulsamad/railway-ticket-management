598bc27f5c3e59a3d42af95f40460ebd6f7a3b20
Abdul Samad Ansari <hello@abdulsamad.dev>
2021-04-01 13:02:23 +0530

    Removed unwanted console.log

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const main = document.querySelector('#main');
-  const loader = document.querySelector('#loader');
-  console.log('fslkdfjsklfj');
-  if (loader && main) {
-    loader.classList.add('d-none');
-    main.classList.remove('d-none');
-  }
+	const main = document.querySelector('#main');
+	const loader = document.querySelector('#loader');
+
+	if (loader && main) {
+		loader.classList.add('d-none');
+		main.classList.remove('d-none');
+	}
 });
