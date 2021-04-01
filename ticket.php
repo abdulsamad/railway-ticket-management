3ee1b889896c40d2d6908f3c0627a767bb752287
Abdul Samad Ansari <hello@abdulsamad.dev>
2021-04-01 14:45:55 +0530

    Added ticket.php

--- ticket.php
+++ ticket.php
@@ -1,7 +1,16 @@
 <?php
 	require ("./db/conn.php");
+	require ("./phpqrcode/qrlib.php");
 	session_start();
 
+	if (!file_exists('temp/')) {
+    mkdir('temp/', 0777, true);
+	}
+
+	$path = 'temp/';
+	$file = $path.uniqid().'.png';
+	$ticketNo = 'Ticket';
+	QRcode::png($ticketNo, $file, 'L', 10, 2);
 
 ?>
 
@@ -28,11 +37,15 @@
 	<div class="d-none" id="main">
 		<nav id="navbar" class="navbar navbar-expand-lg navbar-dark">
 			<div class="container">
-				<a class="navbar-brand" href="#">RailMumbai</a>
+				<a class="navbar-brand" href="./index.php">RailMumbai</a>
 			</div>
 		</nav>
 
 		<div class="container my-2">
+			<a href="./index.php" class="btn btn-primary mt-3 d-inline-flex align-items-center justify-content-center">
+				<svg width="24" height="24" fill-rule="evenodd" clip-rule="evenodd" fill="#fff"><path d="M2.117 12l7.527 6.235-.644.765-9-7.521 9-7.479.645.764-7.529 6.236h21.884v1h-21.883z"/></svg>
+				&nbsp;&nbsp;Back
+			</a>
 			<h1 class="text-center my-3">Ticket</h1>
 			<div class="jumbotron">
 				<div class="row">
@@ -48,11 +61,11 @@
 						<p class="lead"><b class="font-weight-bold">Boarding Time:</b> 2021-05-21 17:58:11</p>
 					</div>
 					<div class="col-md-4 d-flex align-items-center justify-content-center">
-						<img src="https://satyr.io/300x300/2?text=My+Ticket" alt="">
+						<img src="<?php echo $file ?>" alt="Ticket QRCode">
 					</div>
 				</div>
 				<div class="my-2 text-center">
-					<button class="btn btn-primary">Download Ticket</button>
+					<a href="<?php echo $file ?>" class="btn btn-primary" download>Download Ticket</a>
 				</div>
 			</div>
 		</div>
