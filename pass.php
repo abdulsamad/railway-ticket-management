a6ea341c7a8d1b50a498cb4cbec5b64aa0169b8c
Abdul Samad Ansari <hello@abdulsamad.dev>
2021-04-07 18:16:50 +0530

    Changed page title

--- pass.php
+++ pass.php
@@ -26,7 +26,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>RailMumbai LogIn</title>
+	<title>RailMumbai Pass</title>
 	<link rel="shortcut icon" href="assets/favicon.png">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
